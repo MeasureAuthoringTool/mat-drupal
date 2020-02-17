17b5077fdc058dd605c25275981c4c0de120b8d7
Tyler Funk <tyler.funk@semanticbits.com>
2020-02-17 12:21:29 -0600

    Fix PL errors, mute pa11y notices

---
+++ html/themes/custom/emat/local.gulp-config.js
@@ -0,0 +1,25 @@
+/* globals module */
+
+(() => {
+
+  module.exports = {
+    pa11y: {
+      includeNotices: false,
+      includeWarnings: true,
+      ignore: [
+        'WCAG2AA.Principle2.Guideline2_4.2_4_2.H25.2',
+        'WCAG2AA.Principle2.Guideline2_4.2_4_2.H25.1.NoTitleEl',
+        'WCAG2AA.Principle3.Guideline3_1.3_1_1.H57.2',
+        'WCAG2AA.Principle3.Guideline3_2.3_2_1.G107',
+      ],
+      hideElements: '',
+      rootElement: null,
+      rules: [],
+      standard: 'WCAG2AA',
+      wait: 250,
+      actions: [],
+    },
+  };
+
+})();
+
