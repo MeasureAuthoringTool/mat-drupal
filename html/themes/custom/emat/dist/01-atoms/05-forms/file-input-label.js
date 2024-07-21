49349f6319834e6860427a6c6ff9c45f97375bf7
Dmitrii <dmitrii.larionov@icf.com>
2024-07-21 01:15:31 -0500

    [MAT-7438] remove eMat theme

--- html/themes/custom/emat/dist/01-atoms/05-forms/file-input-label.js
+++
@@ -1,6 +0,0 @@
-"use strict";/**
- * @file
- * A JavaScript file containing the main menu functionality (small/large screen)
- *
- */(function(){'use strict';for(var a=document.querySelectorAll(".js-form-type-managed-file"),b=0;b<a.length;b++){var c=a[b],d=c.querySelector(".form-item__file");d.addEventListener("change",function(a){var b=a.target.files[0].name;a.target.parentElement.querySelector(".js-file-box .file-box__label").innerHTML=b})}})();
-//# sourceMappingURL=file-input-label.js.map
