26cbcd24e7bcc7599c3e1baa8b86c315eabb659c
Dmitrii <dmitrii.larionov@icf.com>
2024-10-14 15:51:13 -0500

    [MAT-7230] refactor jquery once usages

--- html/themes/custom/madie/dist/02-molecules/extlink/external-link.js
+++ html/themes/custom/madie/dist/02-molecules/extlink/external-link.js
@@ -1,7 +1,1 @@
-"use strict";/**
- * @file
- * Modifies the onclick behavior of the extlink Drupal mod to
- * use magnific popup rather than the built-in system confirmation
- * dialog.
- */(function(a,b){'use strict';b.behaviors.externalLink={attach:function attach(){a(document).once("mat-external-link").each(function(){drupalSettings.data.extlink.extAlertText&&a("#js-extlink-alert-text").html(drupalSettings.data.extlink.extAlertText),document.querySelector(".js-extlink-continue").addEventListener("click",function(){a.magnificPopup.close()}),document.querySelector(".js-extlink-decline").addEventListener("click",function(b){return b.preventDefault(),a.magnificPopup.close(),!1}),b.extlink.popupClickHandler=function(b,c){var d=c.href;return a("#js-extlink-continue").attr("href",d),a("#js-extlink-preview").html(d),a.magnificPopup.open({items:{src:"#extlink-disclaimer"}}),!1}})}}})(jQuery,Drupal);
-//# sourceMappingURL=external-link.js.map
+((n,i)=>{i.behaviors.externalLink={attach:function(e,t){n(once("mat-external-link",document)).each(function(){drupalSettings.data.extlink.extAlertText&&n("#js-extlink-alert-text").html(drupalSettings.data.extlink.extAlertText),document.querySelector(".js-extlink-continue").addEventListener("click",function(e){n.magnificPopup.close()}),document.querySelector(".js-extlink-decline").addEventListener("click",function(e){return e.preventDefault(),n.magnificPopup.close(),!1}),i.extlink.popupClickHandler=function(e,t){t=t.href;return n("#js-extlink-continue").attr("href",t),n("#js-extlink-preview").html(t),n.magnificPopup.open({items:{src:"#extlink-disclaimer"}}),!1}})}}})(jQuery,Drupal);
