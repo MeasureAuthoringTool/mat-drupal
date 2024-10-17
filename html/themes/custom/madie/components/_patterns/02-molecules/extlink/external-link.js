b3433835885d73f7851ab285711768a9ceb86573
Dmitrii <dmitrii.larionov@icf.com>
2024-10-17 12:34:12 -0500

    [MAT-7230] Upgrade to Drupal 10

--- html/themes/custom/madie/components/_patterns/02-molecules/extlink/external-link.js
+++ html/themes/custom/madie/components/_patterns/02-molecules/extlink/external-link.js
@@ -1,35 +1,35 @@
 /**
  * @file
  * Modifies the onclick behavior of the extlink Drupal mod to
- * use magnific popup rather than the built-in system confirmation
- * dialog.
+ * use magnific popup rather than the built-in system confirmation dialog.
  */
-(function ($, Drupal) {
-
+(function ($, Drupal, once) {
   'use strict';
 
   Drupal.behaviors.externalLink = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
 
-      $(once(
-        'mat-external-link',
-        document
-      )).each(function () {
-        if (drupalSettings.data.extlink.extAlertText) {
-          $('#js-extlink-alert-text').html(drupalSettings.data.extlink.extAlertText);
+      // Use Drupal's once library to apply behavior only once per link.
+      once('mat-external-link', 'a', context).forEach(function (el) {
+
+        // Check for external link alert text.
+        if (settings.data.extlink && settings.data.extlink.extAlertText) {
+          $('#js-extlink-alert-text').html(settings.data.extlink.extAlertText);
         }
 
-        document.querySelector('.js-extlink-continue').addEventListener('click', function(e) {
+        // Attach event listeners for continue and decline buttons.
+        $('.js-extlink-continue').on('click', function (e) {
           $.magnificPopup.close();
         });
 
-        document.querySelector('.js-extlink-decline').addEventListener('click', function(e) {
+        $('.js-extlink-decline').on('click', function (e) {
           e.preventDefault();
           $.magnificPopup.close();
           return false;
         });
 
-        Drupal.extlink.popupClickHandler = function(event, originalLink) {
+        // Define popup click handler for external links.
+        Drupal.extlink.popupClickHandler = function (event, originalLink) {
           var externalHref = originalLink.href;
           $('#js-extlink-continue').attr('href', externalHref);
           $('#js-extlink-preview').html(externalHref);
@@ -40,9 +40,8 @@
           });
           return false;
         };
-
       });
     }
-  }
+  };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
